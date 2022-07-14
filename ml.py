9062a864469021a4d9f53d6227a860f907a169c5
Nicolas Medina <medinanicolas@protonmail.com>
2022-07-14 18:53:48 -0400

    remove trash

--- ml.py
+++
@@ -1,8 +0,0 @@
-from datasets import datasets
-import numpy as np
-import pickle
-size = 100
-dataset = datasets()
-# ds = dataset.create("C:/Users/nico/Dropbox/Coursera/DeepLearning/ml_aplicado/datasets/pikachu", (size,size))
-ds = dataset.create("datasets/pikachu", (size,size))
-pickle.dump(ds, open("aer.pickle","wb"))
